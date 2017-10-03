e2e94336082aef230e07e346b2e6c4d163de879e
aliccce <aliceg@outlook.com>
2017-10-03 18:25:14 +0200

    Lots of clean up and refactoring

--- 2PS/RPS_MPI.c
+++ 2PS/RPS_MPI.c
@@ -42,8 +42,8 @@ int p_my_j_dim;
 int p_my_i_dim;
 
 int p_north, p_south, p_east, p_west;
-int p_border_top, p_border_bottom;
-int p_top, p_bottom;
+int p_border_top, p_border_bottom, p_border_left, p_border_right;
+int p_top, p_bottom, p_left, p_right;
 
 // The dimensions for the process local petri
 int p_local_petri_cols;
@@ -76,7 +76,7 @@ int main(int argc, char** argv){
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  srand( 1234 * rank );
+  srand( 4321 * rank );
 
   ////////////////////////////////
   // Create cartesian communicator
@@ -111,9 +111,12 @@ int main(int argc, char** argv){
   create_types();
 
   for ( int iter = 0; iter < 1000; iter++ ) {
-      local_petri      = iter % 2 == 0 ? local_petri_A : local_petri_B;
+      local_petri = iter % 2 == 0 ? local_petri_A : local_petri_B;
       local_petri_next = iter % 2 == 0 ? local_petri_B : local_petri_A;
+
       iterate_CA();
+      exchange_borders();
+
   }
 
   gather_petri();
@@ -158,69 +161,27 @@ void create_types(){
 
 
 void initialize(){
-  //TODO: assign the following to something more useful than 0
-  p_local_petri_cols = IMG_X / p_j_dims;
+  // Getting rows and columns in petri_dish
   p_local_petri_rows = IMG_Y / p_i_dims;
+  p_local_petri_cols = IMG_X / p_j_dims;
 
-  printf("rows, cols:  %d, %d\n", p_local_petri_rows, p_local_petri_cols);
-
-  // TODO: When allocating these buffers, keep in mind that you might need to allocate a little more
-  // than just your piece of the petri.
+  // Allocate petri_dish with padding
   p_local_petri_size = (2*BORDER_SIZE + p_local_petri_cols) * (2*BORDER_SIZE + p_local_petri_rows);
   local_petri_A = malloc( p_local_petri_size * sizeof(cell) );
   local_petri_B = malloc( p_local_petri_size * sizeof(cell) );
 
-  // TODO: Randomly perturb the local dish. Only perturb cells that belong to your process,
-  // leave border pixels white.
-
-
-  // Make it aaaall white
-  if (rank == 0) {
-
-  for( int i = -BORDER_SIZE; i < p_local_petri_rows + 2*BORDER_SIZE; i++ ) {
-      for( int j = -BORDER_SIZE; j < p_local_petri_cols + 2*BORDER_SIZE; j++ ) {
-          local_petri_A[ pixel(i, j) ].color = 0;
-          local_petri_A[ pixel(i, j) ].strength = 1;
-      }
-  }
-}
-
-/*
-    if (rank == 1) {
-
-    for( int i = -BORDER_SIZE; i < p_local_petri_rows + 2*BORDER_SIZE; i++ ) {
-        for( int j = -BORDER_SIZE; j < p_local_petri_cols + 2*BORDER_SIZE; j++ ) {
-            local_petri_A[ pixel(i, j) ].color = 1;
-            local_petri_A[ pixel(i, j) ].strength = 4;
-        }
-    }
-    }
-
-    if (rank == 2) {
-
-    for( int i = -BORDER_SIZE; i < p_local_petri_rows + 2*BORDER_SIZE; i++ ) {
-        for( int j = -BORDER_SIZE; j < p_local_petri_cols + 2*BORDER_SIZE; j++ ) {
-            local_petri_A[ pixel(i, j) ].color = 2;
-            local_petri_A[ pixel(i, j) ].strength = 2;
-        }
-    }
-  }
-      if (rank == 3) {
+  // Make the petri_dish aaaall white
+    if (rank == 0) {
 
       for( int i = -BORDER_SIZE; i < p_local_petri_rows + 2*BORDER_SIZE; i++ ) {
           for( int j = -BORDER_SIZE; j < p_local_petri_cols + 2*BORDER_SIZE; j++ ) {
-              local_petri_A[ pixel(i, j) ].color = 3;
-              local_petri_A[ pixel(i, j) ].strength = 2;
+              local_petri_A[ pixel(i, j) ].color = 0;
+              local_petri_A[ pixel(i, j) ].strength = 1;
           }
       }
-      }
+    }
 
-*/
-
-
-
-
-  // and then add some seeds ...
+  // and then add some colors ...
   int color, i, j;
 
   for (int iter = 0; iter < p_local_petri_rows; iter++ ) {
@@ -231,90 +192,72 @@ void initialize(){
       local_petri_A[ pixel(i, j) ].strength = 1;
   }
 
-
-  // Calculate border offsets
+  // Calculate border offsets (for receiving borders)
   p_border_top = pixel(-BORDER_SIZE, 0);
   p_border_bottom = pixel(p_local_petri_rows, 0);
+  p_border_left = pixel(0, -BORDER_SIZE);
+  p_border_right = pixel(0, p_local_petri_cols);
 
-  // And calculate inline border offsets (for sending - these might not be needed).
+  // And calculate inline border offsets (for sending).
   p_top = pixel(0, 0);
   p_bottom = pixel(p_local_petri_rows - 1, 0);
+  p_left = pixel(0, 0);
+  p_right = pixel(0, p_local_petri_cols - 1);
 
 }
 
 
 void exchange_borders(){
-    //TODO: Exchange borders inbetween each step
 
-    // Just sending and receiving as a lunatic here. Only ones that need some
-    // post-processing is when sending west or east.
-    // Trying a non-blocking approach ...
+    /* Exchanges borders between processes, by doing non-blocking sending and
+    receiving. Will wait for all incoming data (receiving) to complete before
+    continuing on to the next iteration.
+    */
 
-    MPI_Request cares[2];     // 0 = receive from west, 1 = receive from east
-    MPI_Request dont_cares[6];
-    /* A bit of a weird distinction, but the two arrays are used for waitall
-    and waitany calls later. Basically, we don't need to do any post-processing
-    after sending/receiving to/from the processes in the dont_cares array.
-    It is needed, however, in the cares array (filling columns from west and east). */
-
-    int dc_offset = 0;
-    int dc_length = 6;
-    int c_offset = 0;
-    int c_length = 2;
+    MPI_Request send[4];
+    MPI_Request recv[4];
+    int r_length = 4;
+    int r_index = 0;
 
     if ( p_west != -1 ) {
-        MPI_Isend( local_petri + pixel(0, 0), 1, mpi_col_t, p_west, 1, cart_comm, dont_cares + dc_offset++);
-        MPI_Irecv( local_petri + pixel(0, -BORDER_SIZE), 1, mpi_col_t, p_west, 1, cart_comm, cares + c_offset++ );
-    } else { dc_length--; c_length--; }
+        MPI_Isend( local_petri + p_left, 1, mpi_col_t, p_west, 1, cart_comm, send );
+        MPI_Irecv( local_petri + p_border_left, 1, mpi_col_t, p_west, 1, cart_comm, recv + r_index++ );
+    } else { r_length--; }
 
     if ( p_east != -1 ) {
-        MPI_Isend( local_petri + pixel(0, p_local_petri_cols - 1), 1, mpi_col_t, p_east, 1, cart_comm, dont_cares + dc_offset++ );
-        MPI_Irecv( local_petri + pixel(0, p_local_petri_cols), 1, mpi_col_t, p_east, 1, cart_comm, cares + c_offset++ );
-    } else { dc_length--; c_length--; }
+        MPI_Isend( local_petri + p_right, 1, mpi_col_t, p_east, 1, cart_comm, send + 1 );
+        MPI_Irecv( local_petri + p_border_right, 1, mpi_col_t, p_east, 1, cart_comm, recv + r_index++ );
+    } else { r_length--; }
 
     if ( p_north != -1 ) {
-        MPI_Isend( local_petri + pixel( 0, 0 ), 1, mpi_row_t, p_north, 1, cart_comm, dont_cares + dc_offset++ );
-        MPI_Irecv( local_petri + pixel( -1, 0 ), 1, mpi_row_t, p_north, 1, cart_comm, dont_cares + dc_offset++ );
-    } else { dc_length = dc_length - 2; }
+        MPI_Isend( local_petri + p_top, 1, mpi_row_t, p_north, 1, cart_comm, send + 2 );
+        MPI_Irecv( local_petri + p_border_top, 1, mpi_row_t, p_north, 1, cart_comm, recv + r_index++ );
+    } else { r_length--; }
 
     if ( p_south != -1 ) {
-        MPI_Isend( local_petri + pixel( p_local_petri_rows - 1, 0 ), 1, mpi_row_t, p_south, 1, cart_comm, dont_cares + dc_offset++ );
-        MPI_Irecv( local_petri + pixel( p_local_petri_rows, 0 ), 1, mpi_row_t, p_south, 1, cart_comm, dont_cares + dc_offset++ );
-    } else { dc_length = dc_length - 2; }
+        MPI_Isend( local_petri + p_bottom, 1, mpi_row_t, p_south, 1, cart_comm, send + 3 );
+        MPI_Irecv( local_petri + p_border_bottom, 1, mpi_row_t, p_south, 1, cart_comm, recv + r_index++ );
+    } else { r_length--; }
 
-    while ( c_length > 0 ) {
-        MPI_Status stat;
-        int index;
-        int length = c_length;
-        MPI_Waitany( length, cares, &index, &stat );
-
-        if ( stat.MPI_SOURCE == p_west ) {  /*printf("Rank %d received from left.\n", rank); */}
-        else if ( stat.MPI_SOURCE == p_east ) {  /*printf("Rank %d received from right.\n", rank);*/ }
-        c_length --;
-    }
-
-    MPI_Status stats[dc_length];
-    MPI_Waitall( dc_length, dont_cares, stats );
-
+    MPI_Status stats[r_length];
+    MPI_Waitall( r_length, recv, stats );
 }
 
 
 
 void iterate_CA(){
-  //TODO: Iterate the cellular automata one step
-
+  // Iterate the cellular automata one step
   for( int i = 0; i < p_local_petri_rows; i++ ) {
       for ( int j = 0; j < p_local_petri_cols; j++ ) {
           local_petri_next[ pixel(i, j) ] = next_cell( i, j, local_petri );
       }
   }
-
   local_petri = local_petri_next;
-  exchange_borders();
 }
 
+
 void gather_petri(){
-  //TODO: Gather the final petri for process rank 0
+  // Gather the final petri for process rank 0
 
     if (rank == 0) {
         alloc_img();
@@ -325,7 +268,7 @@ void gather_petri(){
         }
 
         // Then moving on to the other ranks
-        for (int proc = 1; proc < size; proc ++ ) {
+        for (int iter = 1; iter < size; iter ++ ) {
 
             MPI_Status status;
             MPI_Probe( MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status );
@@ -337,6 +280,7 @@ void gather_petri(){
 
             MPI_Recv( buf, count, mpi_cell_t, status.MPI_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
 
+            // Get the coordinates of the process
             int coords[2] ;
             MPI_Cart_coords(cart_comm, status.MPI_SOURCE, 2, coords);
             int proc_i_dim = coords[0];
@@ -344,13 +288,6 @@ void gather_petri(){
 
             int i_offset = p_local_petri_rows * proc_i_dim;
             int j_offset = p_local_petri_cols * proc_j_dim;
-
-
-            printf("(i, j) offset is %d, %d\n", i_offset, j_offset);
-
-            printf("WHAT DO I GET   %d %d\n", coords[0], coords[1]);
-
-
 
             for ( int i = 0; i < p_local_petri_rows; i++ ) {
                 memcpy( image[i_offset + i] + j_offset, buf + pixel( i, 0 ), p_local_petri_cols * sizeof(cell) );
@@ -429,7 +366,7 @@ cell next_cell(int i, int j, cell* petri_dish){
   }
 
   if(next_cell.strength > 6){
-    next_cell.strength = 4;
+    next_cell.strength = 6;
   }
 
   return next_cell;
