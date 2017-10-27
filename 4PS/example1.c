779adb0cbbf7f01617d17ea3c52c2c4c0248f7db
aliccce <aliceg@outlook.com>
2017-10-27 03:20:10 +0200

    profiling

--- 4PS/example1.c
+++ 4PS/example1.c
@@ -17,13 +17,14 @@ int main(int argc, char *argv[])
 
     //IT MIGHT BE HARD TO GET ANY SPEEDUP WITH A NETWORK THIS SMALL! Scale It up
     //Example : genann *ann = genann_init(2, 2, 512, 1);
+    double start, end, start_run, end_run;
 
-    genann *ann = genann_init(2, 1, 5, 1);
-
-    double start, end;
-    start = walltime();
+    genann *ann = genann_init(2, 2, 512, 1);
 
     int trainingIter = 2000;
+
+    start = walltime();
+
     /* Train on the four labeled data points many times. */
     for (i = 0; i < trainingIter; ++i) {
         genann_train(ann, input[0], output + 0, 3);
@@ -35,12 +36,25 @@ int main(int argc, char *argv[])
     end = walltime();
 
     printf("Time for %d training iterations was : %f\n", trainingIter, end-start);
+    printf("Average for each iteration was : %f\n", (end-start)/trainingIter );
 
     /* Run the network and see what it predicts. */
-    printf("Output for [%1.f, %1.f] is %1.f.\n", input[0][0], input[0][1], *genann_run(ann, input[0]));
-    printf("Output for [%1.f, %1.f] is %1.f.\n", input[1][0], input[1][1], *genann_run(ann, input[1]));
-    printf("Output for [%1.f, %1.f] is %1.f.\n", input[2][0], input[2][1], *genann_run(ann, input[2]));
-    printf("Output for [%1.f, %1.f] is %1.f.\n", input[3][0], input[3][1], *genann_run(ann, input[3]));
+    double out0, out1, out2, out3;
+    start_run = walltime();
+
+    out0 = *genann_run(ann, input[0]);
+    out1 = *genann_run(ann, input[1]);
+    out2 = *genann_run(ann, input[2]);
+    out3 = *genann_run(ann, input[3]);
+
+    end_run = walltime();
+
+    printf("Time for running genann_run: %f\n", end_run-start_run );
+
+    printf("Output for [%1.f, %1.f] is %1.f.\n", input[0][0], input[0][1], out0 );
+    printf("Output for [%1.f, %1.f] is %1.f.\n", input[1][0], input[1][1], out1 );
+    printf("Output for [%1.f, %1.f] is %1.f.\n", input[2][0], input[2][1], out2 );
+    printf("Output for [%1.f, %1.f] is %1.f.\n", input[3][0], input[3][1], out3 );
 
     genann_free(ann);
     return 0;
